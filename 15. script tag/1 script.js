2b198484201bcb944b1ae5db5d6d946b05da6dd3
rajun <rajunpandey1234@gmail.com>
2024-08-26 14:36:10 +0545

    learned scripttag in javascript

---
+++ 15. script tag/1 script.js
@@ -0,0 +1,18 @@
+//The <script> tag is used in HTML to include JavaScript code in a webpage. 
+//There are two ways of using it:
+
+
+/*
+//1.Inline: You can write JavaScript code directly inside the <script> tag like this:
+<script>
+    // JavaScript code goes here
+</script>
+*/
+
+
+
+/*
+//2.External: You can use the src attribute of the <script> tag to reference an external JavaScript file like this:
+
+<script src="path/to/your/script.js"></script>
+*/
